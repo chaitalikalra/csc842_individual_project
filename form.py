eecc0519ee0ed6977dbb1df0578d1ad954d136a4
Chaitali Kalra <chaitalikalra@gmail.com>
2019-07-25 01:42:36 -0700

    Pass data to next page

--- form.py
+++ form.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+
+
 app = Flask(__name__)
 
 
@@ -10,7 +13,37 @@ def display_form():
 
 @app.route('/results', methods=['POST'])
 def results():
-    return "Hello"
+    firstname = request.form.get('firstname', '')
+    lastname = request.form.get('lastname', '')
+    email = request.form.get('email', '')
+    address = ' '.join([
+        request.form.get('address1', ''),
+        request.form.get('address2', ''),
+        request.form.get('city', ''),
+        request.form.get('state', ''),
+        request.form.get('zip', ''),
+    ])
+
+    def phone_format(n):
+        try:
+            return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
+        except ValueError:
+            return ""
+
+    phone = phone_format(request.form.get('phoneno', ''))
+    education = request.form.get('education', 'N/A')
+    income = request.form.get('income', 'N/A')
+
+    output = ''
+    output += 'Name: {}\n'.format(firstname + ' ' + lastname)
+    output += 'Email: {}\n'.format(email)
+    output += 'Address: {}\n'.format(address)
+    output += "Phone: {}\n".format(phone)
+    output += "Education: {}\n".format(education)
+    output += "Income: {}\n".format(income)
+
+    return output.replace('\n', '<br />')
+
 
 if __name__ == '__main__':
     app.run()
