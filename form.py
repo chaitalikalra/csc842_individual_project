9a6d8e92c7e0fea04ca441a937f55b3659d37bd5
Chaitali Kalra <chaitalikalra@gmail.com>
2019-07-25 00:32:32 -0700

    recaptcha

--- form.py
+++ form.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def display_form():
     return render_template('form.html')
 
+
+@app.route('/results', methods=['POST'])
+def results():
+    return "Hello"
+
 if __name__ == '__main__':
     app.run()
-
