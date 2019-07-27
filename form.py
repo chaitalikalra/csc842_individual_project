f28e5388421914afaf658b5af432939516414583
Chaitali Kalra <chaitalikalra@gmail.com>
2019-07-26 20:40:49 -0700

    Add results page

--- form.py
+++ form.py
@@ -13,10 +13,12 @@ def display_form():
 
 @app.route('/results', methods=['POST'])
 def results():
+    data = {}
     firstname = request.form.get('firstname', '')
     lastname = request.form.get('lastname', '')
-    email = request.form.get('email', '')
-    address = ' '.join([
+    data['name'] = firstname + ' ' + lastname
+    data['email'] = request.form.get('email', '')
+    data['address'] = ' '.join([
         request.form.get('address1', ''),
         request.form.get('address2', ''),
         request.form.get('city', ''),
@@ -24,25 +26,30 @@ def results():
         request.form.get('zip', ''),
     ])
 
+    data['map_address'] = ' '.join([
+        request.form.get('address1', ''),
+        request.form.get('city', ''),
+        request.form.get('state', ''),
+        request.form.get('zip', ''),
+    ])    
+
     def phone_format(n):
         try:
             return format(int(n[:-1]), ",").replace(",", "-") + n[-1]
         except ValueError:
             return ""
 
-    phone = phone_format(request.form.get('phoneno', ''))
-    education = request.form.get('education', 'N/A')
-    income = request.form.get('income', 'N/A')
+    data['phone'] = phone_format(request.form.get('phoneno', ''))
+    education = request.form.get('education', '')
+    if len(education) == 0:
+        education = 'N/A'
+    data['education'] = education
+    income = request.form.get('income', '')
+    if len(income) == 0:
+        income = 'N/A'
+    data['income'] = income
 
-    output = ''
-    output += 'Name: {}\n'.format(firstname + ' ' + lastname)
-    output += 'Email: {}\n'.format(email)
-    output += 'Address: {}\n'.format(address)
-    output += "Phone: {}\n".format(phone)
-    output += "Education: {}\n".format(education)
-    output += "Income: {}\n".format(income)
-
-    return output.replace('\n', '<br />')
+    return render_template('result.html', data=data)
 
 
 if __name__ == '__main__':
